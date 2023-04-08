cc0b13079f12c9a2c898f68a8af01918e6d6908f
krishnaprasad-1010 <121943430+krishnaprasad-1010@users.noreply.github.com>
2023-04-08 21:38:29 +0530

    servo motor angle pub

---
+++ scripts/servo.py
@@ -0,0 +1,18 @@
+from std_msgs.msg import Int16
+
+rospy.init_node("servo_node")
+
+class Main():
+
+    def __init__(self):
+        self.pub = rospy.Publisher("servo", Int16, queue_size=10)
+        self.rate = rospy.Rate(10)
+
+    def run(self):
+        while not rospy.is_shutdown():
+
+            num = int(input("Enter the angle: "))
+            rospy.loginfo(f"Angle: {num}")
+            self.pub.publish(num)
+            self.rate.sleep()
+
