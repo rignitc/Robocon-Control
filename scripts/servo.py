7059b5e6918226a7ed76b940b20f53b511def1fe
Vivek <vivekpankaj1@gmail.com>
2023-04-09 22:03:00 +0530

    servo control added

--- scripts/servo.py
+++ scripts/servo.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Int16
+from std_msgs.msg import Bool
 import time
 
 rospy.init_node("servo_node")
@@ -10,6 +11,8 @@ class Main():
 
     def __init__(self):
         self.pub = rospy.Publisher("servo", Int16, queue_size=10)
+        self.sub = rospy.Subscriber("flick", Bool, self.callback)
+        self.flick = False
         self.rate = rospy.Rate(10)
 
     def run(self):
@@ -18,12 +21,15 @@ class Main():
             # rospy.loginfo(f"Angle: {num}")
             # self.pub.publish(num)
             # self.rate.sleep()
-            num = input("Press to shoot")
-            rospy.loginfo(f"Shooting")
-            self.pub.publish(90)
-            time.sleep(1)
-            self.pub.publish(0)
+            if self.flick:
+                rospy.loginfo(f"Shooting")
+                self.pub.publish(90)
+                time.sleep(1)
+                self.pub.publish(0)
             self.rate.sleep()
+        
+    def callback(self, flick):
+        self.flick = flick.data
 
 
 if __name__ == "__main__":
