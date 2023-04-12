17ac34573c42eb068dd52e7d5050cf6f3634f07c
krishnaprasad-1010 <121943430+krishnaprasad-1010@users.noreply.github.com>
2023-04-13 01:58:17 +0530

    stepper code for rr

---
+++ arduino/RR/stpper_hori.ino
@@ -0,0 +1,78 @@
+#include "DRV8825.h"
+#include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/Int32.h>
+#define Enable 8
+
+ros::NodeHandle nh;
+
+DRV8825 hstepper;
+DRV8825 vstepper;
+
+const int hDIRECTION_PIN =5;
+const int hSTEP_PIN = 2;
+
+const int vDIRECTION_PIN =6;
+const int vSTEP_PIN = 3;
+
+
+
+void VStepperCB(const std_msgs::Int32& vtrigger){
+    if(vtrigger.data ==1){
+        vstepper.setDirection(DRV8825_COUNTERCLOCK_WISE);
+        for(int i=0;i<100;i++)
+            {
+            vstepper.step();
+            delay(1);
+            }
+
+    }
+    else if(vtrigger.data ==-1){
+        vstepper.setDirection(DRV8825_CLOCK_WISE);
+        for(int i=0;i<100;i++)
+            {
+            vstepper.step();
+            delay(1);
+            }
+
+    }
+}
+
+void HStepperCB(const std_msgs::Int32& htrigger){
+    if(htrigger.data ==1){
+            hstepper.setDirection(DRV8825_COUNTERCLOCK_WISE);
+            for(int i=0;i<100;i++)
+            {
+            hstepper.step();
+            delay(2);
+            }
+
+    }
+    else if(htrigger.data ==-1){
+            hstepper.setDirection(DRV8825_CLOCK_WISE);
+            for(int i=0;i<100;i++)
+            {
+            hstepper.step();
+            delay(2);
+            }
+    }
+}
+
+ros::Subscriber<std_msgs::Int32> vert("screw_power", &VStepperCB);
+ros::Subscriber<std_msgs::Int32> hori("belt_power", &HStepperCB);
+
+void setup()
+{
+    hstepper.begin(hDIRECTION_PIN, hSTEP_PIN);
+    vstepper.begin(vDIRECTION_PIN, vSTEP_PIN);
+    pinMode(Enable,OUTPUT);    //to make enable zero. Its connected to 8 in sheild
+    digitalWrite(Enable,LOW);
+    nh.initNode();
+    nh.subscribe(vert);
+    nh.subscribe(hori);
+}
+void loop()
+{
+    nh.spinOnce();
+    delay(10); 
+}
